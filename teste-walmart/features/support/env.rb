cb6cbceba04612287fa47c35e17c4d7b9dc3a857
LuizRamos <lgramos09@gmail.com>
2015-07-28 14:23:30 -0300

    Teste Processo Seletivo Walmart
    Processo seletivo Walmart para a vaga de Analista de Testes Pleno.
    Automação com cucumber, capybara e HTTParty

---
+++ teste-walmart/features/support/env.rb
@@ -0,0 +1,7 @@
+require 'capybara/cucumber'
+#require 'capybara/poltergeist' #executar modo headless
+ 
+Capybara.default_driver = :selenium 
+
+#executar modo headless
+#Capybara.javascript_driver = :poltergeist 
